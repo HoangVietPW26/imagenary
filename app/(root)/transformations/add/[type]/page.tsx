f05b63ec198008594ebb18f034d2c005d44fe162
HoangVietPW26 <nhviet26@gmail.com>
2025-01-25 13:57:21 +0000

    Image transform layout

---
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -0,0 +1,12 @@
+import Header from '@/components/shared/Header'
+import React from 'react'
+import { transformationTypes } from '@/constants'
+
+const AddTransformationTypePage = ({params: {type}}: SearchParamProps) => {
+  const transformation = transformationTypes[type]
+  return (
+    <Header title={transformation.title} subTitle={transformation.subTitle} />
+  )
+}
+
+export default AddTransformationTypePage
