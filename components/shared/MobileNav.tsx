36d22f85271f2c85a5e96173b24221117d27e6e2
HoangVietPW26 <nhviet26@gmail.com>
2025-01-26 12:51:52 +0000

    allow home page

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -50,7 +50,7 @@ const MobileNav = () => {
 
         <SignedOut>
           <Button asChild className='button bg-purple-gradient bg-cover'>
-              <Link href="/sign-in"></Link>
+              <Link href="/sign-in">Login</Link>
           </Button>
         </SignedOut>
       </nav>
--- middleware.ts
+++ middleware.ts
@@ -2,7 +2,6 @@ import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
 
 
 const protectedRoutes = createRouteMatcher([
-    '/',
     '/profile',
     '/credits',
     '/transformation'
